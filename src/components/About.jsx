ff7e536e96a64d6ed98b0a95feb8b388079bc92b
marguptl <margilpatel57@gmail.com>
2025-01-18 19:34:40 -0500

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import { useGSAP } from '@gsap/react'
+import React from 'react'
+import gsap from 'gsap'
+import AnimatedTitle from './AnimatedTitle'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const About = () => {
+
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        })
+
+        clipAnimation.to('.mask-clip-path',{
+            width: '100vw',
+            height:'100vh',
+            borderRadius: 0
+        })
+    })
+  return (
+    <div id = "about" className='min-h-screen w-screen '>
+      <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+        <h2 className='font-general text-sm uppercase md:text-[20px]'>Welcome To Zentry</h2>
+
+        <AnimatedTitle title="Disc<b>o</b>ver the world's <br /><br /> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center"/>
+
+        <div className='about-subtext'>
+            <p>The game of game begins-your life, now an epic MMORPG</p>
+            <p>Zentry unites every player from countless games and platform</p>
+      </div>
+      </div>
+      <div className='h-dvh w-screen ' id='clip'>
+        <div className='mask-clip-path about-image'>
+            <img src="img/about.webp" alt="background" className='absolute left-0 top-0 size-full object-cover' />
+
+        </div>
+      </div>
+      
+    </div>
+
+    
+  )
+}
+
+export default About
